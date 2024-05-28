eee57dacbb2e66980398c6e5e723f906cf3e2f03
juancarsep <carballosepejuan@gmail.com>
2024-05-28 10:52:41 -0300

    callbacks asincronicos

--- FileSystem/asincronico/callback.js
+++ FileSystem/asincronico/callback.js
@@ -1,8 +1,14 @@
+
+
 const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
 const mult = (a, b) => a * b;
 const div = (a, b) => a / b;
 const mod = (a, b) => a % b;
+
+//
+//======================CALLBACK SINCRONICO======================
+//
 
 
 //Aca pasando las funciones de encima como parametro, me ahorro tener que hacer el switch teniendo un mejor desacople.
@@ -33,3 +39,24 @@ console.log(operacion(n1, n2, resta));
 console.log(operacion(n1, n2, mult));
 console.log(operacion(n1, n2, div));
 console.log(operacion(n1, n2, mod));
+
+
+//
+//======================CALLBACK ASINCRONICO======================
+//
+
+//setTimeOut
+console.log('Inicio delay', new Date().toLocaleString());
+setTimeout(() => console.log('Fin delay', new Date().toLocaleString()), 2000);
+console.log('Otras tareas');
+
+//setInterval, a diferencia de setTimeOut, este se sigue ejecuntando hasta que se le de la orden de parar
+console.log('Inicio delay', new Date().toLocaleString());
+
+const refTimer = setInterval(() => console.log('Fin delay', new Date().toLocaleString()), 2000);
+//Asi se detiene el setInterval, tambien existe el clearTimeOut
+setTimeout(() => {
+    console.log('Deteniendo timer!')
+    clearInterval(refTimer);
+}, 15000)
+
