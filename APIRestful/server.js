6986074b663283a51457de9f000535a5e02a5295
NoxiePC <carballosepejuan@gmail.com>
2024-05-29 16:30:49 -0300

    creacion de api restful con express

---
+++ APIRestful/server.js
@@ -0,0 +1,57 @@
+//
+//      API RESTful de productos
+//
+
+const productos = [
+    { id: 1, nombre: "TV", precio: 12342.43, stock: 55 },
+    { id: 2, nombre: "Compu", precio: 1233.43, stock: 55 },
+    { id: 3, nombre: "Auris", precio: 1235.43, stock: 55 },
+    { id: 4, nombre: "Mouse", precio: 1236.43, stock: 55 },
+    { id: 5, nombre: "Teclado", precio: 123.43, stock: 55 },
+    { id: 6, nombre: "Joystick", precio: 1223.43, stock: 55 },
+]
+
+
+const express = require('express');
+
+const app = express();
+
+
+
+//GET ENDPOINT
+// app.get('/productos', (req, res) => {
+//     res.json({productos})
+// })
+
+
+//Agregandole un signo de pregunta al parametro hace que no sea obligatorio
+app.get('/productos/:id?', (req, res) => {
+    const { id } = req.params
+
+    if (id) {
+        const producto = productos.find(producto => producto.id = id)
+        res.json(producto || {}) // ---> SHORT CIRCUIT OPERATOR
+    }else{
+        res.json(productos)
+    }
+
+//esta modificacion permite que si no se pasa id, no da error y devuelve todos los registros, si se pasa id devuelv el registro
+//con la id solicitada o devulve vacio
+
+
+})
+//POST ENDPOINT
+app.post('/productos', (req, res) => {
+})
+//PUT ENDPOINT
+app.put('/productos', (req, res) => {
+})
+//DELETE ENDPOINT
+app.delete('/productos', (req, res) => {
+})
+
+
+
+const PORT = 8080;
+const server = app.listen(PORT, () => console.log(`Servidor ApiRestful escuchando en http://localhost:${PORT}`));
+server.on('error', error => console.log(`Error en el servidor: ${error.message}`))
